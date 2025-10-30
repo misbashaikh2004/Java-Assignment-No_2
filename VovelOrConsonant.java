1d9b38a13662336229b05fcaa220dfc8d68ae057
misbashaikh2004 <misba6602@gmail.com>
2025-10-30 23:55:16 +0530

    First commit

---
+++ VovelOrConsonant.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class VovelOrConsonant{
+  public static void main(String[] args){
+   
+  char ch;
+ 
+  System.out.println("Enter Any Character"); 
+  Scanner sc=new Scanner(System.in);
+
+  ch=sc.next().charAt(0);
+
+  if(ch=='a' || ch=='e' || ch=='i' || ch=='e' || ch=='u'){
+     System.out.println("Character is Vovel");
+   }
+  else{
+     System.out.println("Character Is Consonant");
+  }
+ }
+}
+  
+  
