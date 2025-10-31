e66d8450cf5137630e77e695cd485dbfe834dbb4
misbashaikh2004 <misba6602@gmail.com>
2025-10-31 19:02:47 +0530

    First commit

---
+++ InterChangeUsingOpe.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class InterChangeUsingOpe{
+  public static void main(String args[]){
+     
+   int a,b;
+   Scanner sc=new Scanner(System.in);
+ 
+   System.out.println("Enter 2 Numbers");
+   a=sc.nextInt();
+   b=sc.nextInt();
+ 
+   a = a * b;
+   b = a / b;
+   a = a / b;
+
+    
+ System.out.println("After Changing The Number:"+ a +" "+b);
+  }
+}
+  
