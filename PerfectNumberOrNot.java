22cd11108a2d846dc5d73d352d44106c895daf10
misbashaikh2004 <misba6602@gmail.com>
2025-11-01 19:50:54 +0530

    First commit

---
+++ PerfectNumberOrNot.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class PerfectNumberOrNot{
+  public static void main(String[] args){
+ 
+   int n,sum=0;
+   Scanner sc=new Scanner(System.in);
+  
+   System.out.println("Enter a Number");
+   n=sc.nextInt();
+   
+   for(int i=1;i<n;i++){
+     if(n % i == 0)
+     sum=sum+i;
+   }
+  
+   if(sum == n)
+      System.out.println("Perfect Number");
+   
+    else
+        System.out.println("Not Perfect Number");
+    }
+}
