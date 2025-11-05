a6773ea8fb6c27a6556e4141d588eab156ffcf89
misbashaikh2004 <misba6602@gmail.com>
2025-11-05 22:12:15 +0530

    First commit

--- VovelOrConsonant.java
+++ VovelOrConsonant.java
@@ -1,10 +1,12 @@
+//8. Accept a lowercase character from the user and check whether the character is a vowel or consonant. (Hint: a,e,i,o,u are vowels)
+
 import java.util.Scanner;
 public class VovelOrConsonant{
   public static void main(String[] args){
    
   char ch;
  
-  System.out.rintln("Enter Any Character"); 
+  System.out.println("Enter Any Character"); 
   Scanner sc=new Scanner(System.in);
 
   ch=sc.next().charAt(0);
